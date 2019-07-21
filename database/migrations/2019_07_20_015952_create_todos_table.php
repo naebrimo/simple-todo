3f5c395c2aeacdc663766fc1ac6fa3a599afe78e
naebrimo <jeffreyfenis@gmail.com>
2019-07-21 19:11:09 +0800

    Create 2019_07_20_015952_create_todos_table.php

---
+++ database/migrations/2019_07_20_015952_create_todos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTodosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('todos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id')->index();
+            $table->string('title');
+            $table->text('description');
+            $table->boolean('active')->default(TRUE);
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('todos');
+    }
+}
