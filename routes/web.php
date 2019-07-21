cc73976e3850d20ec668c9264f9c23e5388399cf
naebrimo <jeffreyfenis@gmail.com>
2019-07-21 19:12:32 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,14 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes(['register' => FALSE]);
+Route::prefix('/home')->group(function(){
+    Route::get('/', 'HomeController@index')->name('home');
+    Route::get('/todo/search', 'TodoController@search')->name('todo.search');
+    Route::get('/todo/sort', 'TodoController@sort')->name('todo.sort');
+    Route::match(['get','post'], '/todo/create/confirm', 'TodoController@confirm')->name('todo.confirm');
+    Route::get('/todo/create/complete', 'TodoController@complete')->name('todo.complete');
+    Route::resource('/todo', 'TodoController');
+
+});
